ca0bd6d9c5478b81dd6eed2b8870f5346a72978c
cgorgulla <c.gorgulla@gmail.com>
2019-03-27 11:39:38 -0400

    Adding contributing information

--- bin/vflp_prepare_inputdb_meta_to_sub.sh
+++ bin/vflp_prepare_inputdb_meta_to_sub.sh
@@ -1,18 +1,29 @@
 input_folder=$1
 set -x
+
 cd $input_folder
+pwd_initial=${PWD}
 
 for metatranch in $(ls -d */); do 
-
     cd $metatranch
     for tranch in $(ls *.tar); do 
-        tar -xvf $tranch
-        cd ${tranch/.tar};
+        tar -xvf $tranch -C /dev/shm
+        cd /dev/shm/${tranch/.tar}
         for collection in $(ls *.tar.gz); do 
             tar -xvzf $collection
+            for smile_file in $(ls ${collection/.*}/*smi); do
+                smile=$(cat $smile_file)
+                smile_file_basename=$(basename $smile_file .smi)
+                echo "$smile ${smile_file_basename}" >> ${collection/.tar.gz}.txt
+            done
+            rm -r ${collection/.*}
+            mkdir -p ${pwd_initial}/${metatranch}/${tranch/.tar}
+            cp ${collection/.tar.gz}.txt ${pwd_initial}/${metatranch}/${tranch/.tar}/
         done
         cd ..
+        rm -r ${tranch/.tar}
+        cd ${pwd_initial}/${metatranch}
     done
     cd ..
 done
-cd ..
+cd ${pwd_initial}/..
