bde980798d4c252eddea55e546b8d071abe88f50
cgorgulla <c.gorgulla@gmail.com>
2020-04-12 09:24:30 -0400

    improved code

--- bin/vfvs_prepare_compoundids2smiles.sh
+++ bin/vfvs_prepare_compoundids2smiles.sh
@@ -7,8 +7,8 @@ The <input_list> has to contain the collection in the first column and the ZINC-
 The colums have to be separated by single spaces.
 All path names have to be relative to the working directory.
 <smiles_collection_folder_format>
-    * tranche: smiles_collection_folder/<tranch>/<collection>.smi
-    * meta_tranche: smiles_collection_folder/<metatranch>/tranch.smi"
+    * tranche: smiles_collection_folder/<tranche>/<collection>.smi
+    * meta_tranche: smiles_collection_folder/<metatranche>/<tranche>.smi"
 
 if [ "${1}" == "-h" ]; then
    echo -e "\n${usage}\n\n"
@@ -46,23 +46,23 @@ output_file="$4"
 
 # Body
 while read -r line; do 
-    tranch=$(echo -n "$line" | awk -F '[_]' '{print $1}')
+    tranche=$(echo -n "$line" | awk -F '[_]' '{print $1}')
     collection=$(echo -n "$line" | awk -F '[_ ]' '{print $2}')
     compound_id=$(echo -n "$line" | awk -F '[ ]' '{print $2}')
-    metatranch=${tranch:0:2}
+    metatranche=${tranche:0:2}
     trap '' ERR
     if [ "${smiles_folder_format}" == "tranche" ]; then
-        smiles=$(grep -w "${compound_id/_T*}" ${smiles_folder}/${tranch}/${collection}.* | awk '{print $1}')
+        smiles=$(grep -w "${compound_id/_T*}" ${smiles_folder}/${tranche}/${collection}.* | awk '{print $1}')
     elif [ "${smiles_folder_format}" == "meta_tranche" ]; then
-        smiles=$(grep -w "${compound_id/_T*}" ${smiles_folder}/${metatranch}/${tranch}.* | awk '{print $1}')
+        smiles=$(grep -w "${compound_id/_T*}" ${smiles_folder}/${metatranche}/${tranche}.* | awk '{print $1}')
     fi
     
     if [ -n "${smiles}" ]; then
         echo "${smiles} ${compound_id}" >> ${output_file}
-        echo "Compound ${compound_id} of collection ${collection} successfully extracted"
+        echo "Compound ${compound_id} of collection ${tranche}_${collection} successfully extracted"
     else 
         echo ${compound_id} ${collection} >> ${output_file}.failed
-        echo "Compound ${compound_id} of collection ${collection} failed to extract"
+        echo "Compound ${compound_id} of collection ${tranche}_${collection} failed to extract"
     fi
     trap 'error_response_nonstd $LINENO' ERR
 done < "${input_file}"
