1feafb109c2d0897fc288b811d740c71df093936
Christoph Gorgulla <c.gorgulla@gmail.com>
2021-04-15 16:30:45 -0400

    Increasing robustness

--- bin/vfvs_pp_extract_best_poses.sh
+++ bin/vfvs_pp_extract_best_poses.sh
@@ -34,13 +34,13 @@ mkdir -p $3
 index=0
 while IFS= read -r line; do
     index=$((index+1))
-   read -r -a array <<< "$line"
-   collection=${array[0]}
-   tranch=${array/_*}
-   collection_id=${array/*_}
-   zinc_id="${array[1]}"
-   minindex="${array[3]}"
-   echo "Extracting $tranch, $collection_id, $zinc_id, $minindex"
-   cp $2/${tranch}/${collection_id}/${zinc_id}/replica-${minindex}/${zinc_id}.rank-1.pdb $3/${index}_${zinc_id}.pdb || true
-   echo
+    read -r -a array <<< "$line"
+    collection=${array[0]}
+    tranch=${array/_*}
+    collection_id=${array/*_}
+    zinc_id="${array[1]}"
+    minindex="${array[3]}"
+    echo "Extracting $tranch, $collection_id, $zinc_id, $minindex"
+    cp $2/${tranch}/${collection_id}/${zinc_id}/replica-${minindex}/${zinc_id}.rank-1.pdb $3/${index}_${zinc_id}.pdb || true
+    echo
 done < $1
